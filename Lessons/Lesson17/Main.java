dd467d33581668fe62d10420bdc3649a0dbb6d1e
Vitamin_68 <wetalm68@gmail.com>
2019-09-27 04:02:53 +0300

    Lesson 17

--- Lessons/Lesson17/Main.java
+++ Lessons/Lesson17/Main.java
@@ -14,39 +14,48 @@ public class Main {
     public static final String separatorAnd = "&";
     public static final String fullurl =
             jdbc
-            + hostAndPort
-            + url
-            + separator
-            + unicode
-            + separatorAnd
-            + jdbcTimezoneShift
-            + separatorAnd
-            + dateTime
-            + separatorAnd
-            + serverTimezone;
+                    + hostAndPort
+                    + url
+                    + separator
+                    + unicode
+                    + separatorAnd
+                    + jdbcTimezoneShift
+                    + separatorAnd
+                    + dateTime
+                    + separatorAnd
+                    + serverTimezone;
     public static final String USER_NAME = "root";
     public static final String password = "root";
     public static final String insertPerson = "insert into person (name,age) values (?,?);";
 
 
     public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager.getConnection(url);
+        Connection connection = DriverManager.getConnection(fullurl, USER_NAME, password);
+
 
         Statement statement = connection.createStatement();
-        statement.execute("create table if not exist person " +
-                "id int primari key auto increment, " +
-                "name varchar(255)," +
-                "married boolean defoult false");
-        statement.execute("insert into person (id,name,married) value (2,'Timmy',false)");
-        statement.execute("update person set name = 'Karl', married=true where id = 1");
-        statement.execute("delete from person where name = 'Timmy'");
-        statement.execute("after table person add age int");
+        statement.execute("create table if not exists person(" +
+                "id int primary key auto_increment, " +
+                "name varchar(255), " +
+                "married boolean default false)");
+//        statement.execute("insert into person (id,name,married) value (2,'Timmy',false)");
+//        statement.execute("update person set name = 'Karl', married=true where id = 2");
+//        statement.execute("delete from person where name = 'Timmy'");
+//        statement.execute("alter table person add age int");
+
+        /*
+        по умолчанию у statement параметр setAutoCommit = true, т.е.
+        statement.setAutoCommit(true), и все execute выполняются сразу
+        но если его поставить в false (отложить выполнение), то они не выполнятся, пока не дать команду
+        connection.commit();
+        чтобы не дергать постоянно БД, а выполнить несколько операций за 1 раз
+        */
 
         PreparedStatement preparedStatement = connection.prepareStatement(
                 "insert into person (name,married,age) value(?,?,?)");
 
-        preparedStatement.setString(1,"Slava");
-        preparedStatement.setBoolean(2,false);
+        preparedStatement.setString(1, "Slava");
+        preparedStatement.setBoolean(2, false);
         preparedStatement.setInt(3, 12);
         preparedStatement.execute();
 
@@ -55,15 +64,14 @@ public class Main {
         ResultSet set = statement.getResultSet();
         while (set.next()) {
             System.out.println(
-                    "ID: " + set.getInt(1) +
-                            "name: " + set.getString(2) +
-                            "married: " + set.getBoolean(3) +
-                            "age: " + set.getInt(4));
+                    "ID: " + set.getInt(1) + ",  " +
+                            " name: " + set.getString(2) + ",  " +
+                            " married: " + set.getBoolean(3) + ",  " +
+                            " age: " + set.getInt(4));
 
 
         }
-
+        statement.close();
         connection.close();
-
     }
 }
